40b9dd28aba53dafe8366410ce8f920e2aafcb2b
Vũ Khắc Huy <khuy02032005@gmail.com>
2026-09-11 16:31:38 +0700

    Hoàn thành bài TH02_FirstWeb - NameList

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -24,6 +24,7 @@ namespace TH02_FirstWeb.Controllers
 
         public IActionResult NameList()
         {
+
             List<string> danhSachTen = new List<string>
             {
                 "Nguyễn Văn A",
